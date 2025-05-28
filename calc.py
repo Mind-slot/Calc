a3630a8e07f65e78ce784cd76627a59f71d0a634
Mindslot <mmindslot@gmail.com>
2025-05-27 20:50:47 -0500

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,48 +1,79 @@
-def add(x, y):
-    return x + y
+import time 
+time.sleep(1)
+print("Python Calculator \n\n\n")
+time.sleep(2.5)
 
-def subtract(x, y):
-    return x - y
+def calculator():
+   var1 = input("Enter first number:  ")
+   var2 = input("Enter second number:  ")
+   if (checkIfInt(var1) and checkIfInt(var2)):
+       var1 = int(var1)
+       var2 = int(var2)
+       if (userInput == "*"):
+           print("The product of the first and second number is " + str(mul(var1, var2)))
+       elif (userInput == "/"):
+           print("The first number divided by the second number is " + str(div(var1, var2)))
+       elif (userInput == "+"):
+           print("The sum of the first and second numbers is " + str(add(var1, var2)))
+       elif(userInput == "-"):
+           print("The first number minus the second number is " + str(sub(var1, var2)))
+       else:
+           print("Invalid input.")
+           time.sleep(2)
+           main()
+   else:
+       print("Both inputs must be integers.\nTaking you back to the calculator...")
+       time.sleep(2)
+       calculator()
+def add(num1, num2):
+   return(num1 + num2)
+def mul(num1, num2):
+   return(num1 * num2)
+def div(num1, num2):
+   return(num1 / num2)
+def sub(num1, num2):
+   return(num1 - num2)
 
-def multiply(x, y):
-    return x * y
+def checkIfInt(possibleInt):
+   try:
+       val = int(possibleInt)
+       return True
+   except ValueError:
+       return False
+def again():
+   repeat = input("Would you like to use the calculator again?  ").lower()
+   if(repeat == "y" or repeat == "yes"):
+       main()
+   elif(repeat == "n" or repeat == "no"):
+       print("See you next time!")
+       time.sleep(2)
+       quit()
+   else:
+       while(True):
+           print("Sorry, invalid input.")
+           time.sleep(1)
+           again()
+def main():
+   global userInput
+   userInput = input("Which mathematical function would you like to do? Insert *, /, +, or -. Type HELP for help.  ")
+   if(userInput == "HELP"):
+       print("\n* = Multiplication\n/ = Division\n+ = Addition\n- = Subtraction")
+       secondaryInput = input("\nType \"BACK\" to go back to the calculator.  ")
+       if (secondaryInput == "BACK"):
+           main()
+       else:
+           while(True):
+               backInput = input("Invalid input. Please type \"BACK\" to return to the calculator.  ")
+               if(backInput == "BACK"):
+                   main()
+    
+   elif (userInput != "*" and userInput != "/" and userInput != "+" and userInput != "-"):
+       print("Invalid input!")
+       time.sleep(2)
+       main()
+   else:
+       calculator()
+   again()
+main()
 
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
-
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid input")
+"""
